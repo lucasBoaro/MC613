55e885591664c79bef32b678262a12ff4319ac09
FernandoPen <f281180@dac.unicamp.br>
2026-03-22 12:35:40 -0300

    fix erros sintaxe e fix SelecionarValor

--- VendingMachine/selecionarValor.vhd
+++ VendingMachine/selecionarValor.vhd
@@ -3,7 +3,6 @@ use ieee.std_logic_1164.all;
 
 entity selecionarValor is
     port (
-        KEY_CONFIRM : in  std_logic;                    
         BIN_SWITCH  : in  std_logic_vector(5 downto 0); 
         BIN_VALOR   : out std_logic_vector(7 downto 0) := (others => '0')
     );
@@ -11,20 +10,12 @@ end entity selecionarValor;
 
 architecture behavior of selecionarValor is
 begin
-    process(KEY_CONFIRM)
-    begin
-        -- Detecta o momento em que o botão é solto (Active Low na DE0-CV)
-        if rising_edge(KEY_CONFIRM) then 
-            case BIN_SWITCH is
-                when "000001" => BIN_VALOR <= "00000101"; -- R$ 0,05 (5 em decimal)
-                when "000010" => BIN_VALOR <= "00001010"; -- R$ 0,10 (10)
-                when "000100" => BIN_VALOR <= "00011001"; -- R$ 0,25 (25)
-                when "001000" => BIN_VALOR <= "00110010"; -- R$ 0,50 (50)
-                when "010000" => BIN_VALOR <= "01100100"; -- R$ 1,00 (100)
-                when "100000" => BIN_VALOR <= "11001000"; -- R$ 2,00 (200)
-                when others   => BIN_VALOR <= (others => '0');
-            end case;
-        end if;
-    end process;
-
+    with BIN_SWITCH select
+        BIN_VALOR <= "00000101" when "000001", -- R$ 0,05
+                     "00001010" when "000010", -- R$ 0,10
+                     "00011001" when "000100", -- R$ 0,25
+                     "00110010" when "001000", -- R$ 0,50
+                     "01100100" when "010000", -- R$ 1,00
+                     "11001000" when "100000", -- R$ 2,00
+                     "00000000" when others;
 end architecture;
