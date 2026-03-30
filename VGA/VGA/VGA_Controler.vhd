5980ae2bb5ac56aa5cdf8167f8e0ed75604e823a
Gabriel Saia Schade <g281202@dac.unicamp.br>
2026-03-30 16:13:16 -0300

    Criando o controler VGA e top level

--- VGA/VGA/VGA_Controler.vhd
+++ VGA/VGA/VGA_Controler.vhd
@@ -1,38 +1,79 @@
-LIBRARY IEEE;
-USE IEEE.STD_LOGIC_1164.ALL;
-USE IEEE.NUMERIC_STD.ALL;
+  LIBRARY IEEE;
+  USE IEEE.STD_LOGIC_1164.ALL;
+  USE IEEE.NUMERIC_STD.ALL;
 
-ENTITY VGA_Controller IS
-  port (
-    -- Entradas de Controle de Clock e Reset
-    pixel_clk    : IN  STD_LOGIC;                     -- Clock de 25.175 MHz gerado pelo PLL
-    reset_n      : IN  STD_LOGIC;                     -- Reset assíncrono (ativo baixo)
+  ENTITY VGA_Controller IS
+    port (
+      -- Entradas de Controle de Clock e Reset
+      pixel_clk    : IN  STD_LOGIC;                     -- Clock de 25.175 MHz gerado pelo PLL
+      reset_n      : IN  STD_LOGIC;                     -- Reset assíncrono (ativo baixo)
 
-    -- Entradas de Cor (vindos da PPU)
-    r_in         : IN  STD_LOGIC_VECTOR(7 DOWNTO 0);  -- Intensidade do vermelho do pixel atual
-    g_in         : IN  STD_LOGIC_VECTOR(7 DOWNTO 0);  -- Intensidade do verde do pixel atual
-    b_in         : IN  STD_LOGIC_VECTOR(7 DOWNTO 0);  -- Intensidade do azul do pixel atual
+      -- Entradas de Cor (vindos da PPU)
+      r_in         : IN  STD_LOGIC_VECTOR(7 DOWNTO 0);  -- Intensidade do vermelho do pixel atual
+      g_in         : IN  STD_LOGIC_VECTOR(7 DOWNTO 0);  -- Intensidade do verde do pixel atual
+      b_in         : IN  STD_LOGIC_VECTOR(7 DOWNTO 0);  -- Intensidade do azul do pixel atual
 
-    -- Saídas de Controle Interno (enviados para a PPU)
-    pixel_x      : OUT STD_LOGIC_VECTOR(9 DOWNTO 0);  -- Coordenada X atual
-    pixel_y      : OUT STD_LOGIC_VECTOR(9 DOWNTO 0);  -- Coordenada Y atual
-    video_active : OUT STD_LOGIC;                     -- '1' se estiver dentro da área visível (Active Video)
+      -- Saídas de Controle Interno (enviados para a PPU)
+      pixel_x      : OUT STD_LOGIC_VECTOR(9 DOWNTO 0);  -- Coordenada X atual
+      pixel_y      : OUT STD_LOGIC_VECTOR(9 DOWNTO 0);  -- Coordenada Y atual
+      video_active : OUT STD_LOGIC;                     -- '1' se estiver dentro da área visível (Active Video)
 
-    -- Saídas Físicas (conectadas aos pinos da DE1-SoC)
-    VGA_R        : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);  -- Saída VGA Vermelha
-    VGA_G        : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);  -- Saída VGA Verde
-    VGA_B        : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);  -- Saída VGA Azul
-    VGA_HS       : OUT STD_LOGIC;                     -- Sincronismo Horizontal
-    VGA_VS       : OUT STD_LOGIC;                     -- Sincronismo Vertical
-    VGA_BLANK_N  : OUT STD_LOGIC;                     -- Fora da área visível (ou seja, deve ser '0' no blanking)
-    VGA_SYNC_N   : OUT STD_LOGIC;                     -- Sincronização de vídeo (fixo em '1')
-    VGA_CLK      : OUT STD_LOGIC                      -- Clock do pixel (espelho do pixel_clk)
-  );
-END VGA_Controller;
+      -- Saídas Físicas (conectadas aos pinos da DE1-SoC)
+      VGA_R        : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);  -- Saída VGA Vermelha
+      VGA_G        : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);  -- Saída VGA Verde
+      VGA_B        : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);  -- Saída VGA Azul
+      VGA_HS       : OUT STD_LOGIC;                     -- Sincronismo Horizontal
+      VGA_VS       : OUT STD_LOGIC;                     -- Sincronismo Vertical
+      VGA_BLANK_N  : OUT STD_LOGIC;                     -- Fora da área visível (ou seja, deve ser '0' no blanking)
+      VGA_SYNC_N   : OUT STD_LOGIC := '1';              -- Sincronização de vídeo (fixo em '1')
+      VGA_CLK      : OUT STD_LOGIC                      -- Clock do pixel (espelho do pixel_clk)
+    );
+  END VGA_Controller;
 
-architecture Behavioral of VGA_Controller is
-  
-  signal h_counter : integer range 0 to 799 := 0;  -- Contador horizontal (0-799)
-  signal v_counter : integer range 0 to 523 := 0;  -- Contador vertical (0-523)
+  architecture Behavioral of VGA_Controller is
 
-  
+    signal x_counter : integer range 0 to 799 := 0;  
+    signal y_counter : integer range 0 to 523 := 0;  
+
+  begin
+
+    process(pixel_clk, reset_n)
+    begin
+      if reset_n = '0' then
+        x_counter <= 0;
+        y_counter <= 0;
+      elsif rising_edge(pixel_clk) then
+        
+        if x_counter = 799 then
+          x_counter <= 0;
+          
+          if y_counter = 523 then
+            y_counter <= 0; 
+          else
+            y_counter <= y_counter + 1;
+          end if;
+          
+        else
+          x_counter <= x_counter + 1; 
+        end if;
+        
+      end if;
+    end process;
+
+    VGA_HS <= '0' when (x_counter >= 656 and x_counter <= 751) else '1';
+
+    VGA_VS <= '0' when (y_counter >= 491 and y_counter <= 492) else '1';
+
+    VGA_BLANK_N <= '0';
+    VGA_SYNC_N <= '1';
+    VGA_CLK    <= pixel_clk;
+
+    VGA_R <= x"FF" when (x_counter <= 400 and y_counter <= 300) else x"00";
+    VGA_G <= x"00";
+    VGA_B <= x"00"; 
+
+    pixel_x <= std_logic_vector(to_unsigned(x_counter, 10));
+    pixel_y <= std_logic_vector(to_unsigned(y_counter, 10));
+    video_active <= '1' when (x_counter <= 639 and y_counter <= 479) else '0';
+
+  end Behavioral;
