db9776d7cec1d2f6657718ae683ca2cef0b861cf
JoaoFelipeDM <j258477@dac.unicamp.br>
2026-05-12 17:35:33 -0300

    Finzalizando controlador

--- DRAM/dramController.vhd
+++ DRAM/dramController.vhd
@@ -7,8 +7,8 @@ ENTITY dramController is
         clk_143 : IN STD_LOGIC; -- Clk adaptado para a frequencia da dram
         rst : IN STD_LOGIC; --Valor que reinicializa/inicializa a memória
         address : IN STD_LOGIC_VECTOR(25 DOWNTO 0); -- Endereço de leitura/escrita
-        data_in : IN STD_LOGICVECTOR(7 DOWNTO 0); --Dado para escrita
-        data_out : OUT STD_LOGICVECTOR(7 DOWNTO 0); -- Dado que foi lido
+        data_in : IN STD_LOGIC_VECTOR(7 DOWNTO 0); --Dado para escrita
+        data_out : OUT STD_LOGIC_VECTOR(7 DOWNTO 0); -- Dado que foi lido
         req : IN STD_LOGIC; -- Indica se há requisição
         ready : OUT STD_LOGIC; -- Indica que o controlador pode receber uma requisição
         write_in : IN STD_lOGIC;
@@ -22,9 +22,9 @@ ENTITY dramController is
         DRAM_CKE : OUT STD_lOGIC;
         DRAM_LDQM : OUT STD_lOGIC;
         DRAM_UDQM : OUT STD_lOGIC;
-        DRAM_BA : OUT STD_lOGIC_VECTOR (1 DOWNTO 0)
-        
-    )
+        DRAM_BA : OUT STD_lOGIC_VECTOR (1 DOWNTO 0);
+        DRAM_DQ : INOUT STD_LOGIC_VECTOR(15 DOWNTO 0) -- INOUT porque os pinos são bidirecionais (Recebimento e envio de dados na mesma pinagem) 
+    );
 END dramController;
 
 architecture behavior of dramController is
@@ -34,11 +34,16 @@ architecture behavior of dramController is
     SIGNAL IS_READY : STD_LOGIC := '0';
     SIGNAL block_idle : STD_LOGIC := '0';
 
+    SIGNAL completed : STD_LOGIC := '1'; --Indica se a última requisição foi concluida ou não
+    SIGNAL input_data : STD_LOGIC_VECTOR(7 DOWNTO 0);
+    SIGNAL action : STD_LOGIC; -- 1 para Write e 0 para read
+    SIGNAL save_addr : STD_LOGIC_VECTOR(25 DOWNTO 0);
+
     
     
 
 	-- Build an enumerated type for the state machine
-	type state_type is (reset, init, preChargeAll, autoRefresh_Init, LMR, idle, act, read, write, precharge, refresh, NOP);
+	type state_type is (reset_st, init, preChargeAll, autoRefresh_Init, LMR, idle, act, read_st, write_st, precharge, refresh, NOP);
 
 	-- Register to hold the current state
 	signal state : state_type;
@@ -46,64 +51,88 @@ architecture behavior of dramController is
 
 begin
     ready <= IS_READY;
-	process (clk, reset)
+	process (clk_143, rst)
 	begin
-		if rising_edge(reset) then
-			state <= reset;
+		if rst = '1' then
+			state <= reset_st;
 
-		elsif (rising_edge(clk)) then
+		elsif (rising_edge(clk_143)) then
+            data_out <= DRAM_DQ(7 downto 0);
+
 			case state is
-				when reset=> 
+				when reset_st=> 
 						state <= init;
 				when init=>
-                    counter = 28572;
+                    counter <= 28572;
                     state <= NOP;
                     next_state <= preChargeAll;
 				when preChargeAll=>
-                    counter = 4;
+                    counter <= 4;
                     state <= NOP;
                     next_state <= autoRefresh_Init;
 
 				when autoRefresh_Init=>
                     if counter_refresh = 8 then
                         state <= LMR;
+                    else
+                        counter_refresh <= counter_refresh + 1;
+                        counter <= 9;
+                        state <= NOP;
+                        next_state <= autoRefresh_Init;
                     end if;                    
-                    counter_refresh += 1;
-                    counter = 9
-                    state <= NOP
-                    next_state <= autoRefresh_Init;
 
                 when LMR=>
-                    counter = 2;
+                    counter <= 2;
                     state <= NOP;
                     next_state <= idle;
 
                 when idle=>
-                    IS_READY <= '1';
-                    
                     if block_idle = '0' then
-                        if req = '1' then
+                        IS_READY <= '1';
+                        if completed = '0' then
                             state <= act;
                         end if;
+                    else
+                        IS_READY <= '0';
+                        state <= refresh; -- Ao invés de mexer nos pinos no outro processo, nós transicionamos o estado pra cá.
                     end if;
 
-                when NOP=>
-                    if counter =< 1 then
-                        state <= next_state;
-                    else
-                        counter -= 1;
-                    end if;
                 when act=>
-                    counter = 3;
+                    counter <= 3;
+                    if action = '1' then
+                        next_state <= write_st;
+                    elsif action = '0' then
+                        next_state <= read_st;
+                    end if;
                     state <= NOP;
 
-                    if write_in = '1' then
-                        next_state <= write;
-                    elsif read_in = '1' then
-                        next_state <= read;
-                    end if;
+                when write_st=>
+                    counter <= 2;
+                    next_state <= precharge;
+                    state <= NOP;
 
-                when write=>
+                when read_st=>
+                    counter <= 2;
+                    next_state <= precharge;
+                    state <= NOP;
+
+                when precharge=>
+                    counter <= 3;
+                    next_state <= idle;
+                    state <= NOP;
+                    completed <= '1';
+
+                when refresh=>
+                    counter <= 9;
+                    next_state <= idle;
+                    state <= NOP;
+
+                when NOP=>
+                    if counter <= 1 then
+                        state <= next_state;
+                    else
+                        counter <= counter - 1;
+                    end if;
                     
 			end case;
 
@@ -112,17 +141,17 @@ begin
 
 	-- Determine the output based only on the current state
 	-- and the input (do not wait for a clock edge).
-	process (state, input)
+	process (state, save_addr, input_data)
 	begin
+        --Valores default
+        DRAM_LDQM <= '1';
+        DRAM_UDQM <= '1';
+        DRAM_CKE <= '1';
+        DRAM_ADDR <= (others => '0');
+        DRAM_BA <= "00";
+        DRAM_DQ <= (others => 'Z');
+
 			case state is
-				when init=>
-                        DRAM_CS_N <= '0';
-                        DRAM_RAS_N <= '1';
-                        DRAM_CAS_N <= '1';
-                        DRAM_WE_N <= '1';
-                        DRAM_LDQM <= '1';
-                        DRAM_UDQM <= '1';
-                        DRAM_CKE <= '1';
 				when preChargeAll=>
 					    DRAM_CS_N <= '0';
                         DRAM_RAS_N <= '0';
@@ -141,35 +170,80 @@ begin
                         DRAM_WE_N <= '0';
                         DRAM_BA <= "00";
                         DRAM_ADDR <= "0000000110000"; --M4 e M5 setados iguais a 1
+                when act=>
+                        DRAM_CS_N <= '0';
+                        DRAM_RAS_N <= '0';
+                        DRAM_CAS_N <= '1';
+                        DRAM_WE_N <= '1';
+                        DRAM_BA   <= save_addr(24 downto 23); -- 2 bits de banco
+                        DRAM_ADDR <= save_addr(22 downto 10); -- 13 bits de linha
+                when read_st=>
+                        DRAM_CS_N <= '0';
+                        DRAM_RAS_N <= '1';
+                        DRAM_CAS_N <= '0';
+                        DRAM_WE_N <= '1';
+                        DRAM_ADDR(10) <= '0';
+                        DRAM_BA   <= save_addr(24 downto 23);
+                        DRAM_ADDR(9 downto 0) <= save_addr(9 downto 0); -- 10 bits de coluna
+
+                when write_st=>
+                        DRAM_CS_N <= '0';
+                        DRAM_RAS_N <= '1';
+                        DRAM_CAS_N <= '0';
+                        DRAM_WE_N <= '0';
+                        DRAM_ADDR(10) <= '0';
+                        DRAM_BA   <= save_addr(24 downto 23);
+                        DRAM_ADDR(9 downto 0) <= save_addr(9 downto 0); -- 10 bits de coluna
+                        DRAM_DQ(7 downto 0)  <= input_data; 
+                        DRAM_DQ(15 downto 8) <= (others => '0');
+
                 when NOP=>
                         DRAM_CS_N <= '0';
                         DRAM_RAS_N <= '1';
                         DRAM_CAS_N <= '1';
                         DRAM_WE_N <= '1';
+                when refresh=>
+                        DRAM_CS_N <= '0';
+                        DRAM_RAS_N <= '0';
+                        DRAM_CAS_N <= '0';
+                        DRAM_WE_N <= '1';
+                when others => 
+                        null;
 			end case;
 	end process;
 
-    process (clk) is
-        begin
-            counterAutoRefresh += 1;
+    process (clk_143)
+    begin
+        if rising_edge(clk_143) then
+            counterAutoRefresh <= counterAutoRefresh + 1;
             if IS_READY = '1' and counterAutoRefresh >= 1000 then
                 block_idle <= '1';
-                DRAM_CS_N <= '0';
-                DRAM_RAS_N <= '0';
-                DRAM_CAS_N <= '0';
-                DRAM_WE_N <= '1';
-                counterAutoRefresh = 0;
+                counterAutoRefresh <= 0;
             end if;
 
-            if block_idle = 1 and counterAutoRefresh = 9 then
+            if block_idle = '1' and counterAutoRefresh = 9 then
                 block_idle <= '0';
-                counterAutoRefresh = 0;
+                counterAutoRefresh <= 0;
             end if;
+        end if;
+    end process;
+    
+    process (clk_143)
+    begin
+        if rising_edge(clk_143) then
+            if req = '1' and IS_READY = '1' then
+                completed <= '0';
+                input_data <= data_in;
+                save_addr <= address;
 
-            
+                if write_in = '1' then
+                    action <= '1';
+                elsif read_in = '1' then
+                    action <= '0';
+                end if;
+            end if;
+        end if;
+    end process;
+    
 
-end rtl;
-
-        
-
-    BEGIN
+end behavior;
